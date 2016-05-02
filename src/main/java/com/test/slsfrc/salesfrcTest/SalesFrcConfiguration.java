cc75dda3bf6408663d9ca123f131bf1944edb949
Matus M <Matus M>
2016-05-02 11:56:11 +0200

    commit of changes in salesfrc connector

--- src/main/java/com/test/slsfrc/salesfrcTest/SalesFrcConfiguration.java
+++ src/main/java/com/test/slsfrc/salesfrcTest/SalesFrcConfiguration.java
@@ -5,6 +5,7 @@ import java.security.DomainCombiner;
 import org.identityconnectors.common.StringUtil;
 import org.identityconnectors.common.security.GuardedString;
 import org.identityconnectors.framework.spi.AbstractConfiguration;
+import org.identityconnectors.framework.spi.ConfigurationProperty;
 import org.identityconnectors.framework.spi.StatefulConfiguration;
 
 public class SalesFrcConfiguration extends AbstractConfiguration implements StatefulConfiguration {
@@ -17,6 +18,56 @@ public class SalesFrcConfiguration extends AbstractConfiguration implements Stat
 	
 	private GuardedString refreshToken = null;
 
+	@ConfigurationProperty(order = 1, displayMessageKey = "domain.display",
+            groupMessageKey = "basic.group", helpMessageKey = "domain.help", required = true,
+            confidential = false)
+	
+	public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+    
+    @ConfigurationProperty(order = 2, displayMessageKey = "clientid.display",
+            groupMessageKey = "basic.group", helpMessageKey = "clientid.help", required = true,
+            confidential = false)
+    
+    public String getClientId(){
+    	return clientId;
+    	
+    }
+    
+    public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	
+    @ConfigurationProperty(order = 3, displayMessageKey = "clientsecret.display",
+            groupMessageKey = "basic.group", helpMessageKey = "clientsecret.help", required = true,
+            confidential = true)
+    
+    public GuardedString getClientSecret() {
+        return clientSecret;
+    }
+    
+    public void setClientSecret(GuardedString clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+    
+    @ConfigurationProperty(order = 4, displayMessageKey = "refreshtoken.display",
+            groupMessageKey = "basic.group", helpMessageKey = "refreshtoken.help", required = true,
+            confidential = true)
+    public GuardedString getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(GuardedString refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+    
+    
+	
 	@Override
 	public void validate() {
 
--- src/main/java/com/test/slsfrc/salesfrcTest/SalesfrcConnector.java
+++ src/main/java/com/test/slsfrc/salesfrcTest/SalesfrcConnector.java
@@ -1,13 +1,17 @@
 package com.test.slsfrc.salesfrcTest;
 
+import java.util.Collection;
 import java.util.Set;
 
 import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.Name;
 import org.identityconnectors.framework.common.objects.ObjectClass;
 import org.identityconnectors.framework.common.objects.OperationOptions;
 import org.identityconnectors.framework.common.objects.ResultsHandler;
 import org.identityconnectors.framework.common.objects.Schema;
 import org.identityconnectors.framework.common.objects.Uid;
+import org.identityconnectors.framework.common.objects.filter.AndFilter;
+import org.identityconnectors.framework.common.objects.filter.EqualsFilter;
 import org.identityconnectors.framework.common.objects.filter.Filter;
 import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
 import org.identityconnectors.framework.spi.Configuration;
@@ -29,6 +33,19 @@ SearchOp<Filter>, TestOp, UpdateOp {
     public static final ObjectClass ALIAS = new ObjectClass("Alias");
     
     public static final ObjectClass LICENSE_ASSIGNMENT = new ObjectClass("LicenseAssignment");
+    
+    
+    
+    public static final String ALIASES_ATTR = "aliases";
+    
+    public static final String ORG_UNIT_PATH_ATTR = "orgUnitPath";
+    
+    public static final String GROUP_KEY_ATTR = "groupKey";
+    
+    public static final String EMAIL_ATTR = "email";
+
+    public static final String ALIAS_ATTR = "alias";
+    
     
     private SalesFrcConfiguration configuration; 
 	
@@ -89,8 +106,58 @@ SearchOp<Filter>, TestOp, UpdateOp {
 	@Override
 	public void executeQuery(ObjectClass objectClass, Filter query, ResultsHandler arg2, OperationOptions options) {
 		//final Set<String> attributesToGet = getAttributesToGet(objectClass, options);
-      //  Attribute key = getKeyFromFilter(objectClass, query);
+        Attribute key = getKeyFromFilter(objectClass, query);
 		
 	}
+	
+	protected Attribute getKeyFromFilter(ObjectClass objectClass, Filter filter) {
+        Attribute key = null;
+        if (filter instanceof EqualsFilter) {
+            // Account, Group, OrgUnit object classes
+            Attribute filterAttr = ((EqualsFilter) filter).getAttribute();
+            if (filterAttr instanceof Uid) {
+                key = filterAttr;
+            } else if (ObjectClass.ACCOUNT.equals(objectClass) || ObjectClass.GROUP.equals(objectClass)
+                    && (filterAttr instanceof Name || filterAttr.getName().equalsIgnoreCase(ALIASES_ATTR))) {
+                key = filterAttr;
+            } else if (ORG_UNIT.equals(objectClass) && filterAttr.getName().equalsIgnoreCase(ORG_UNIT_PATH_ATTR)) {
+                key = filterAttr;
+            }
+        } else if (filter instanceof AndFilter) {
+            // Member object class
+            if (MEMBER.equals(objectClass)) {
+                Attribute groupKey = null;
+                Attribute memberKey = null;
+                StringBuilder memberId = new StringBuilder();
+
+                Collection<Filter> filters = ((AndFilter) filter).getFilters();
+                for (Filter f : filters) {
+                    if (f instanceof EqualsFilter) {
+                        Attribute filterAttr = ((EqualsFilter) f).getAttribute();
+                        if (filterAttr.getName().equalsIgnoreCase(GROUP_KEY_ATTR)) {
+                            groupKey = filterAttr;
+                        } else if (filterAttr.getName().equalsIgnoreCase(EMAIL_ATTR) || filterAttr.getName().
+                                equalsIgnoreCase(ALIAS_ATTR) || filterAttr instanceof Uid) {
+                            memberKey = filterAttr;
+                        } else {
+                            throw new UnsupportedOperationException(
+                                    "Only AndFilter('groupKey','memberKey') is supported");
+                        }
+                    } else {
+                        throw new UnsupportedOperationException(
+                                "Only AndFilter('groupKey','memberKey') is supported");
+                    }
+                }
+                if (memberKey != null && groupKey != null) {
+                    memberId.append(groupKey.getValue().get(0));
+                    memberId.append("/");
+                    memberId.append(memberKey.getValue().get(0));
+                    key = new Uid(memberId.toString());
+                }
+            }
+        }
+        return key;
+    }
+
 
 }

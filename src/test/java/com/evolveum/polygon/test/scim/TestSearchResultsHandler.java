7680f5767d48cfbf0be702306361fffd11c658dc
Matus M <Matus M>
2016-08-15 16:07:54 +0200

    addition of handler class

---
+++ src/test/java/com/evolveum/polygon/test/scim/TestSearchResultsHandler.java
@@ -0,0 +1,35 @@
+package com.evolveum.polygon.test.scim;
+
+import java.util.ArrayList;
+
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.objects.ConnectorObject;
+import org.identityconnectors.framework.common.objects.SearchResult;
+import org.identityconnectors.framework.spi.SearchResultsHandler;
+
+
+
+final class TestSearchResultsHandler implements SearchResultsHandler {
+
+	private static final ArrayList<ConnectorObject> result = new ArrayList<>();
+	
+	private static final Log LOGGER = Log.getLog(TestSearchResultsHandler.class);
+	
+	
+	@Override
+	public boolean handle(ConnectorObject connectorObject) {
+		result.add(connectorObject);
+		return true;
+	}
+
+	@Override
+	public void handleResult(SearchResult result) {
+		LOGGER.info("im handling {0}", result.getRemainingPagedResults());
+
+	}
+	
+	public ArrayList<ConnectorObject> getResult(){
+		
+		return this.result;
+	}
+}
